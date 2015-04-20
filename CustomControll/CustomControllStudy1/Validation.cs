f486117820598ca6bb82c973ee0910a7db8bca3a
Yoshinori.N <nayo-msoft@outlook.jp>
2015-04-21 00:15:19 +0900

    カスタムコントロールのサンプル追加

---
+++ CustomControll/CustomControllStudy1/Validation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomControllStudy1
+{
+    class Validation
+    {
+        /// <summary>
+        /// 数値かどうか検証するメソッド
+        /// </summary>
+        public static bool IsNurmeric(object x)
+        {
+            double o;
+            return double.TryParse(x.ToString(), out o);
+        }
+    }
+}
