e64517c4a200ae129b29c37d4b2298b82fd9bfee
Yoshinori.N <nayo-msoft@outlook.jp>
2015-07-14 00:54:57 +0900

    ジェネリックについて記述。

---
+++ Basics/Contents/Generic/Generic/Generic/MyFIFO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class MyFIFO<T>
+    {
+        private int size = 0;
+        private T[] items;
+
+        public MyFIFO()
+        {
+            items = new T[10];
+        }
+
+        public void Push(T x)
+        {
+            items[size++] = x;
+        }
+
+        public T Pop()
+        {
+            T y = items[0];
+            //LINQで配列を作り直す。
+            items = items.Skip(1).ToArray();
+            return y;
+        }
+    }
+}
